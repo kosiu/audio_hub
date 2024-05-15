52bc768533f9aa1024a8a030b7570d1603f6e48a
Jacek Kosek <jacek.kosek@gmail.com>
2024-05-15 06:20:21 +0200

    generating radio list from json

--- static/main.js
+++ static/main.js
@@ -5,44 +5,89 @@ function set_initial_theme() {
         theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     }
     document.documentElement.setAttribute('data-bs-theme', theme)
-    document.getElementById('theme').checked = (theme == 'dark')? true : false;
+    document.getElementById('theme').checked = (theme == 'dark') ? true : false;
 };
-set_initial_theme();
+//set_initial_theme();
 
-function dark_theme(btn){
+function dark_theme(btn) {
     var theme = (btn.checked == true) ? 'dark' : 'light';
     document.documentElement.setAttribute('data-bs-theme', theme)
-    localStorage.setItem('theme',theme);
+    localStorage.setItem('theme', theme);
 };
 
-function more(btn){
-    if (btn.checked){btn.labels[0].innerHTML = "Pokaż mniej";}
-    else            {btn.labels[0].innerHTML = "Pokaż więcej";}
+function more(btn) {
+    if (btn.checked) { btn.labels[0].innerHTML = "Pokaż mniej"; }
+    else { btn.labels[0].innerHTML = "Pokaż więcej"; }
     const hidden_element = document.querySelector('.collapse')
     if ((hidden_element.clientHeight > 0 && !btn.checked) ||
-        (hidden_element.clientHeight == 0 && btn.checked)){
-            new bootstrap.Collapse('#hidden_part')
+        (hidden_element.clientHeight == 0 && btn.checked)) {
+        new bootstrap.Collapse('#hidden_part')
     }
 }
 
 // Comunication with server functions ------------------------------
-function send_action(action) {fetch('/set?action='+action.value)}
-function send_volume(volume) {fetch('/set?volume='+volume.value)}
 
-function set_ui(data){
+function generateRadioList(data) {
+    const list_end = document.getElementById("end_radio_list");
+    const parent = list_end.parentElement
+    //const button_x = document.getElementById("radio_x");
+    //const label_x = document.getElementById("label_x");
+    data.forEach((item, index) => {
+        //const button = button_x.cloneNode(true);
+        //button.setAttribute('id', 'radio' + index);
+        //button.setAttribute('value', index);
+
+        //const label = label_x.cloneNode(true);
+        //label.setAttribute('id', 'label' + index);
+        //label.setAttribute('for', 'radio' + index);
+        //label.innerHTML = `<i class="bi-broadcast-pin"></i> ${item[0]}`;
+        const radioInput = document.createElement('input');
+        radioInput.setAttribute('class', 'btn-check');
+        radioInput.setAttribute('type', 'radio');
+        radioInput.setAttribute('name', 'state');
+        radioInput.setAttribute('id', 'radio' + index);
+        radioInput.setAttribute('value', index);
+        radioInput.setAttribute('onclick', 'send_action(this);');
+
+        const label = document.createElement('label');
+        label.setAttribute('class', 'btn btn-outline-primary');
+        label.setAttribute('for', 'radio' + index);
+        label.innerHTML = `<i class="bi-broadcast-pin"></i> ${item[0]}`;
+
+        parent.insertBefore(radioInput, list_end);
+        parent.insertBefore(label, list_end);
+    });
+    //label_x.remove();
+    //button_x.remove();
+}
+//function radioList() {
+//fetch('/get_radios')
+//    .then((response) => response.json())
+//    .then((data) => generateRadioList(data));
+//}
+//radioList();
+
+function set_ui(data) {
     document.main.volume.value = data.volume;
-    document.main.state.value  = data.input;
+    document.main.state.value = data.input;
 };
+
+function send_action(action) { fetch('/set?action=' + action.value) }
+function send_volume(volume) { fetch('/set?volume=' + volume.value) }
 
 // Server Sent Events (SSE)
 const evtSource = new EventSource("/update");
-evtSource.addEventListener("update_ui", (event) => {set_ui(JSON.parse(event.data));});
+evtSource.addEventListener("update_ui", (event) => { set_ui(JSON.parse(event.data)); });
 
+// After loading page
 document.addEventListener('DOMContentLoaded', () => {
+    set_initial_theme();
+    fetch('/get_radios')
+        .then((response) => response.json())
+        .then((data) => generateRadioList(data));
     more(document.getElementById('show_more'))
-
     fetch('/get')
-    .then((response) => response.json())
-    .then((data) => set_ui(data));
+        .then((response) => response.json())
+        .then((data) => set_ui(data));
 }, false);
 
